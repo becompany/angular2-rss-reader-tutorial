7408695911851ea8c56531915b2a72db5554a31d
Javier Puerto <javier.puerto@becompany.ch>
2016-09-15 12:04:14 +0200

    Load all the needed resources for the application in main app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,33 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Material design.
+import { MdCardModule } from '@angular2-material/card';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule, MdIconRegistry } from '@angular2-material/icon';
+
 import { AppComponent } from './app.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { FeedService } from './feed.service';
+import { StripHtmlTagsPipe } from './pipe/strip-html-tags.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedCardComponent,
+    StripHtmlTagsPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdIconModule
   ],
-  providers: [],
+  providers: [FeedService, MdIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
