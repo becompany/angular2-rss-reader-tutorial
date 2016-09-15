1ca2776d824042021c3ca9b7a5863773440f43c2
Javier Puerto <javier.puerto@becompany.ch>
2016-09-15 12:02:16 +0200

    Add feed component to display RSS entries.

---
+++ src/app/feed-card/feed-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-card',
+  templateUrl: './feed-card.component.html',
+  styleUrls: ['./feed-card.component.css']
+})
+export class FeedCardComponent implements OnInit {
+
+  @Input() feed: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
