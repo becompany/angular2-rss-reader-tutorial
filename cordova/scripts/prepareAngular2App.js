e79837742755c48b1afe1e5ca170ea9e2753059b
Javier Puerto <javier.puerto@becompany.ch>
2016-09-27 13:21:02 +0200

    Script to build and prepare Angular 2 application for Apache Cordova.

---
+++ cordova/scripts/prepareAngular2App.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+
+module.exports = function(context) {
+    console.log('Building Angular 2 application into "./www" directory.');
+    const basePath = context.opts.projectRoot;
+    const baseWWW = basePath + '/www';
+
+    console.log(execSync(
+      "ng build --target=production --environment=prod --output-path cordova/www/ --base-href .",
+      {
+        maxBuffer: 1024*1024,
+        cwd: basePath + '/..'
+      }).toString('utf8')
+    );
+    var files = fs.readdirSync(baseWWW);
+    for (var i = 0; i < files.length; i++) {
+      if (files[i].endsWith('.gz')) {
+        fs.unlinkSync(baseWWW + '/' + files[i]);
+      }
+    }
+};
