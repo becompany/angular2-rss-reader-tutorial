54660dd8d8ed9bed0d013baacbb313db26bf62c1
Javier Puerto <javier.puerto@becompany.ch>
2017-05-08 13:30:22 +0200

    Fix tests runtime error. Add extension ".ts" MIME type.

--- karma.conf.js
+++ karma.conf.js
@@ -36,6 +36,9 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    mime: {
+        'text/x-typescript': ['ts','tsx']
+    }
   });
 };
